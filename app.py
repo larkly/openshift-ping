527809f0d8ce82c486f13dfca2fe1666622b8813
x10an14 <x10an14@hotmail.com>
2016-10-29 10:24:32 +0200

    Feature: Lot's of new features! Python3! Timeouts! Improved error handling!
    Changes included in this 'super-commit':
    1. Function docstrings added. (Module docstring still missing).
    2. subprocess.Popen() now also captures stderr.
       2.a subprocess.Popen() now uses communicate(), which awaits process completion.
       2.b subprocess.Popen().communicate() now also permits timeout limitation.
    3. subprocess.Popen() now returns unicode strings instead of ugly byte-strings!
    4. Flattened some cyclomatic-complexity (AKA reduced it, but just by peanuts).
    5. Now PEP8 compliant! (Except for missing module docstring).

--- app.py
+++ app.py
@@ -5,29 +5,53 @@ import subprocess
 
 app = Flask(__name__)
 
-def filter_output(output, host):
-    o_filter = ""
-    for line in output.stdout:
-        o_filter = o_filter+line
 
-    return_data = "%s" % host + " " +  o_filter
+def filter_output(proc, host, time_limit=None):
+    """Function for getting output from process executed."""
+    try:
+        stdout, stderr = proc.communicate(timeout=time_limit)
+    except subprocess.TimeoutExpired:
+        proc.kill()
+        stdout, stderr = proc.communicate(timeout=time_limit)
+    return_data = "{} {}".format(host, stdout)
+
+    if not stderr:  # => if stderr != "":
+        # Everything is jolly good, ignore stderr
+        return return_data
+
+    return_data += "\n\nstderr:\n{}".format(stderr)
     return return_data
+
 
 @app.route('/')
 def index():
+    """Root index for this Flask webapp. Simply redirects to /ping."""
     return redirect("/ping", code=302)
+
 
 @app.route('/ping')
 @app.route('/ping/<host>')
 def ping(host=None):
-    if host is None:
-        return render_template('results.html', return_data='You need to enter an IP address at the end of the URL, like <domain>/ping/8.8.8.8')
-    else:
-        cmd = "ping -c 4 %s" % host
-        output = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
-        return_data = filter_output(output, host)
-        return render_template('results.html', return_data=return_data)
+    """Main function where the 'magic' happens!..."""
+    if not host:
+        return render_template(
+            'results.html',
+            return_data=(
+                'You need to enter an IP address at the end of the URL,'
+                ' like <domain>/ping/8.8.8.8'))
+
+    # Default time_limit of hardcoded 16 seconds.
+    #   (It's a 4-packet ping for crying out loud...)
+    cmd, time_limit = "ping -c 4".split(" "), 16
+    proc = subprocess.Popen(
+        args=cmd.append(host),
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        universal_newlines=True)
+
+    return_data = filter_output(proc=proc, host=host, time_limit=time_limit)
+    return render_template('results.html', return_data=return_data)
 
 if __name__ == '__main__':
     app.debug = True
-    app.run(host='0.0.0.0',port=8080)
+    app.run(host='0.0.0.0', port=8080)
